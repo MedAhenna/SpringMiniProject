dfce1ad951701fb80d16c6667560f95c7f6e4885
boulguant <boulgant2000@gmail.com>
2022-05-09 11:06:47 +0100

    sm changes

--- src/main/java/com/example/miniprojet/entities/Commande.java
+++ src/main/java/com/example/miniprojet/entities/Commande.java
@@ -1,4 +1,40 @@
 package com.example.miniprojet.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Date;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Commande {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private Date DateCreated;
+    private Integer Qty;
+    private String status;
+
+    @ManyToOne
+    @JsonIgnore
+    private Produit product;
+
+    @ManyToOne
+    @JsonIgnore
+    private Client client;
+
+    @OneToOne
+    @JsonIgnore
+    private InformationDeLivraison informations;
+    public Commande(Date dateCreated, Integer qty) {
+        DateCreated = dateCreated;
+        Qty = qty;
+    }
 }
--- src/main/java/com/example/miniprojet/entities/InformationDeLivraison.java
+++ src/main/java/com/example/miniprojet/entities/InformationDeLivraison.java
@@ -1,31 +1,34 @@
 package com.example.miniprojet.entities;
 
-
-
-import javax.persistence.Entity;
-import javax.persistence.*;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+
+import javax.persistence.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class Client {
+public class InformationDeLivraison {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nom;
     private String prenom;
+    private String address;
+    private Integer telephone;
 
 
-    public Client(String nom, String prenom) {
+
+    public InformationDeLivraison(String nom, String prenom, String address, Integer telephone) {
         this.nom = nom;
         this.prenom = prenom;
+        this.address = address;
+        this.telephone = telephone;
     }
 }

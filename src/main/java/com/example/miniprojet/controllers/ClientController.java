7d78c89cfb5ec673bbfe44cd78d904fd70e786d9
MedAhenna <ahenna.12@gmail.com>
2022-05-09 09:22:08 +0100

    sds

--- src/main/java/com/example/miniprojet/controllers/ClientController.java
+++ src/main/java/com/example/miniprojet/controllers/ClientController.java
@@ -1,10 +1,8 @@
 package com.example.miniprojet.controllers;
 
 import com.example.miniprojet.entities.Client;
-import com.example.miniprojet.services.ClientService;
+import com.example.miniprojet.services.implementations.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
